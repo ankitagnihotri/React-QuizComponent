8b31861a3e9259711661a4f35e1006beffce6a45
Ankit Agnihotri <42108516+ankitagnihotri@users.noreply.github.com>
2018-08-11 16:33:20 +0530

    Add files via upload

--- src/Quiz.js
+++ src/Quiz.js
@@ -1,38 +1,27 @@
-import React, { Component } from 'react';
-import QuizQuestion from './QuizQuestion.js';
-import QuizEnd from './QuizEnd.js';
+import React, { Component } from 'react'
+import QuizQuestion from './QuizQuestion.js'
+import QuizEnd from './QuizEnd.js'
 
-let quizData = require('./quiz_data.json');
+let quizData = require('./quiz_data.json')
 
 class Quiz extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {quiz_position: 1}
-    }
+	constructor(props) {
+		super(props)
+		this.state = { quiz_position: 1 }
+	}
+	render() {
+		const isQuizEnd = ((this.state.quiz_position - 1) === 
+			quizData.quiz_questions.length)
+		
+		return (
+			<div>
+				{isQuizEnd ? <QuizEnd /> : 
+				<QuizQuestion quiz_question={quizData.quiz_questions[this.state.quiz_position - 1]} />}
+			</div>
 
-    handleResetClick() {
-        this.setState({quiz_position: 1});    
-    }
+		)
+	}
 
-    showNextQuestion() {
-        this.setState((state) => {
-            return {quiz_position: state.quiz_position + 1}
-        }) 
-    }
-
-    render() { 
-        let finalComponent = null
-        const isQuizEnd = (this.state.quiz_position - 1) === quizData.quiz_questions.length ? true : false;
-        if (isQuizEnd)
-            finalComponent = <QuizEnd resetClickHandler={this.handleResetClick.bind(this)}/>
-        else 
-            finalComponent = <QuizQuestion quiz_question={quizData.quiz_questions[this.state.quiz_position - 1]} showNextQuestionHandler={this.showNextQuestion.bind(this)}/>
-
-        return ( <div>
-                    {finalComponent}    
-                </div> 
-        );
-    }
 }
- 
-export default Quiz;
+
+export default Quiz
